147f9d316f960f32160bc74758f73afca947d808
Quang9119 <nguyenminhquangquangquang@gmail.com>
2024-10-15 01:21:35 +0700

    Reapply "add UI get all store"
    This reverts commit a57d9f3e91ac4507a264af680c8392ecd406ae41.

--- src/addresses/addresses.controller.ts
+++ src/addresses/addresses.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Param,
+  Patch,
+  Delete,
+  Body,
+  Query,
+} from '@nestjs/common';
+import { CreateAddressDto } from './dtos/create-address.dto';
+import { UpdateAddressDto } from './dtos/update-address.dto';
+import { GetAddressDto } from './dtos/get-address.dto';
+import { AddressService } from './addresses.service';
 
-@Controller('addresses')
-export class AddressesController {}
+@Controller('/address')
+export class AddressController {
+  constructor(private readonly addressService: AddressService) {}
+
+  @Post('/')
+  async createAddress(@Body() body: CreateAddressDto) {
+    return this.addressService.createAddress(body);
+  }
+
+  @Get('/')
+  async getAddresses(@Query() query: GetAddressDto) {
+    return this.addressService.getAddresses(query);
+  }
+
+  @Get('/:id')
+  async getAddressById(@Param('id') id: number) {
+    return this.addressService.getAddressById(id);
+  }
+
+  @Patch('/:id')
+  async updateAddress(@Param('id') id: number, @Body() body: UpdateAddressDto) {
+    return this.addressService.updateAddress(id, body);
+  }
+
+  @Delete('/:id')
+  async removeAddress(@Param('id') id: number) {
+    return this.addressService.removeAddress(id);
+  }
+}
